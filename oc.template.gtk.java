d8a574a99b099245a736dcd13fa903fb16b0be18
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2021-04-05 15:18:54 +0200

    change java to 16

--- oc.template.gtk.java
+++ oc.template.gtk.java
@@ -1,12 +1,12 @@
 ARG TAG=dev
 FROM abcdesktopio/oc.template.gtk:$TAG
 MAINTAINER Alexandre DEVELY 
+ENV DEBIAN_FRONTEND noninteractive
 
-
-RUN DEBIAN_FRONTEND=noninteractive add-apt-repository ppa:linuxuprising/java
-RUN DEBIAN_FRONTEND=noninteractive echo oracle-java15-installer shared/accepted-oracle-license-v1-2 select true | /usr/bin/debconf-set-selections
-RUN DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y --no-install-recommends \
-        oracle-java15-installer         \
+RUN add-apt-repository ppa:linuxuprising/java
+RUN echo oracle-java15-installer shared/accepted-oracle-license-v1-2 select true | /usr/bin/debconf-set-selections
+RUN apt-get update && apt-get install -y --no-install-recommends \
+        oracle-java16-installer         \
         gsfonts-x11                     \
         java-common                     \
-        oracle-java15-set-default
+        oracle-java16-set-default
