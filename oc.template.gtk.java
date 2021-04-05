1fce78768570900825f38bb7fa049545d2a3e3d0
alexandre devely <alexandre.devely@orange.com>
2021-04-05 16:04:09 +0200

    change --install-recommends

--- oc.template.gtk.java
+++ oc.template.gtk.java
@@ -5,8 +5,9 @@ ENV DEBIAN_FRONTEND noninteractive
 
 RUN add-apt-repository ppa:linuxuprising/java
 RUN echo oracle-java16-installer shared/accepted-oracle-license-v1-2 select true | /usr/bin/debconf-set-selections
-RUN apt-get update && apt-get install -y --no-install-recommends \
+#
+# oracle-java16-installer with --install-recommends also installs oracle-java16-set-default
+RUN apt-get update && apt-get install -y --install-recommends \
         oracle-java16-installer         \
         gsfonts-x11                     \
         java-common                     \
-        oracle-java16-set-default
