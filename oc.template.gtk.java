d55a1c486d54065a6a386f560a19e1d9feccbcca
alexandredevely <12896316+alexandredevely@users.noreply.github.co>
2021-11-09 14:16:16 +0100

    add new root oc.template

--- oc.template.gtk.java
+++ oc.template.gtk.java
@@ -2,12 +2,6 @@ ARG TAG=dev
 FROM abcdesktopio/oc.template.gtk:$TAG
 MAINTAINER Alexandre DEVELY 
 ENV DEBIAN_FRONTEND noninteractive
-
-RUN add-apt-repository ppa:linuxuprising/java
-RUN echo oracle-java16-installer shared/accepted-oracle-license-v1-2 select true | /usr/bin/debconf-set-selections
-#
-# oracle-java16-installer with --install-recommends also installs oracle-java16-set-default
 RUN apt-get update && apt-get install -y --install-recommends \
-        oracle-java16-installer         \
-        gsfonts-x11                     \
-        java-common                     \
+        default-jre         \
+        gsfonts-x11                     
