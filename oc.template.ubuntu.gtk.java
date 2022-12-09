1ac10e48f1a2e0890e2eb19865d4c93d499d2796
alexandredevely <12896316+alexandredevely@users.noreply.github.co>
2022-12-09 11:11:55 +0100

    support 3.0

---
+++ oc.template.ubuntu.gtk.java
@@ -0,0 +1,9 @@
+ARG TAG=dev
+ARG BASE_IMAGE
+FROM ${BASE_IMAGE}:$TAG
+MAINTAINER Alexandre DEVELY 
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get update && apt-get install -y --install-recommends \
+        default-jre	\
+        gsfonts-x11   	\
+    && rm -rf /var/lib/apt/lists/*	
